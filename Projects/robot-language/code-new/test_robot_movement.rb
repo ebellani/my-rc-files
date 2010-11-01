92d23dfe61f19fa81eb7b34b4837fa1e256aa88c
Eduardo Bellani <ebellani@gmail.com>
2010-11-01 12:58:43 -0200

    improvemnets using treetop

--- Projects/robot-language/code-new/test_robot_movement.rb
+++ Projects/robot-language/code-new/test_robot_movement.rb
@@ -11,13 +11,15 @@ class TestMovement < Test::Unit::TestCase
   def test_basic_functions
     assert @a_10_by_10_board.eql? RobotMovement::Board.new(9, 9)
     assert @initial_robot.eql? RobotMovement::Robot.new(0, 0, :s)
-
   end
 
   def test_commands
-    assert_not_nil RobotMovement.parser.parse('hello robot')
-    assert_nil RobotMovement.parser.parse('hello chomsky')
-    assert_nil RobotMovement.parser.parse('WRONG INPUT!')
+    assert_nil RobotMovement.parser.parse('hello robot')
+    assert_nil RobotMovement.parser.parse("10")
+
+    assert RobotMovement.parser.parse("1   5").value.instance_of?(RobotMovement::Board)
+    assert @a_10_by_10_board.eql?(RobotMovement.parser.parse("10 10").value)
+
     #     (check-equal? (process-mech (open-input-string "20 40\n2 0 S\nT 0 2\nMMLMLM"))
     #               (make-mech 1 1 #\N))
     #  
